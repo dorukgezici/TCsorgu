5572b9468c1de0ce6b6507960c56839fe522fffb
dorukgezici <dorukgezici@gmail.com>
2017-12-18 03:41:17 +0300

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='TCsorgu',
+      version='0.1',
+      description='TC Identity Number Check',
+      url='http://github.com/dorukgezici/TCsorgu',
+      author='Doruk Gezici',
+      author_email='dorukgezici@gmail.com',
+      license='MIT',
+      packages=['TCsorgu'],
+      zip_safe=False)
